35102bb9b0f06f90e72fdcb28b0604f52232e17c
unknown <greggarcia426@gmail.com>
2023-12-16 22:15:57 -0700

    removed gelt from url

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,28 +11,28 @@ const Navbar = () => {
     <div className="shadow-black-500 right-0 flex w-full justify-between bg-[#0b0c14] p-4 text-gray-300 shadow-lg">
       <ul className="right-0 top-0 hidden w-[70%] justify-between md:flex ">
         <li>
-          <Link to="/gelt/">Overview</Link>
+          <Link to="/">Overview</Link>
         </li>
         <li>
-          <Link to="/gelt/transactions"> Transcations</Link>
+          <Link to="/transactions"> Transcations</Link>
         </li>
         <li>
-          <Link to="/gelt/credit-score">Credit Score</Link>
+          <Link to="/credit-score">Credit Score</Link>
         </li>
         <li>
-          <Link to="/gelt/bills">Bills</Link>
+          <Link to="/bills">Bills</Link>
         </li>
         <li>
-          <Link to="/gelt/budgets">Budgets</Link>
+          <Link to="/budgets">Budgets</Link>
         </li>
         <li>
-          <Link to="/gelt/goals">Goals</Link>
+          <Link to="/goals">Goals</Link>
         </li>
         <li>
-          <Link to="/gelt/trends">Trends</Link>
+          <Link to="/trends">Trends</Link>
         </li>
         <li>
-          <Link to="/gelt/investments">Investments</Link>
+          <Link to="/investments">Investments</Link>
         </li>
       </ul>
       <div onClick={handleMobile} className="z-10 cursor-pointer md:hidden">
